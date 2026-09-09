a1513016b0c5269d97b7454a0facae5a84398c7b
harshil thummar <justv030@gmail.com>
2026-09-09 18:21:39 +0530

    update hardhat config sepolia networ and put info in env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "hardhat/config";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
   solidity: {
     version: "0.8.34",
+  },
+
+  networks: {
+    sepolia: {
+      type: "http",
+      url: process.env.RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
   },
 });
